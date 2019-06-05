bfab6b2957bebedd02f0bee347aa36eb57e90466
Ryan Welcher <me@ryanwelcher.com>
2019-06-05 07:20:38 -0400

    Adds basic examples for plugin-sidebar and plugin-more-menu-item

--- src/slots/basic/index.js
+++ src/slots/basic/index.js
@@ -1,7 +1,7 @@
 import './plugin-sidebar';
 import './plugin-more-menu-item';
-import './plugin-sidebar-more-menu-item';
-import './plugin-post-status-info';
-import './plugin-block-settings-menu-item';
-import './plugin-pre-publish-panel';
-import './plugin-post-publish-panel';
+// import './plugin-sidebar-more-menu-item';
+// import './plugin-post-status-info';
+// import './plugin-block-settings-menu-item';
+// import './plugin-pre-publish-panel';
+// import './plugin-post-publish-panel';
