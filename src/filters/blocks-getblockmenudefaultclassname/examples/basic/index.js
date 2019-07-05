db62307310a4fb4f5aed57fb0f844e4d03445b73
Ryan Welcher <me@ryanwelcher.com>
2019-07-05 13:01:17 -0400

    Removes unused filters

--- src/filters/blocks-getblockmenudefaultclassname/examples/basic/index.js
+++
@@ -1,7 +0,0 @@
-const { addFilter } = wp.hooks;
-
-const filterMenuDefaultClassName = ( classname, blockName ) => {
-	return classname + ' custom-class';
-};
-
-addFilter( 'blocks.getBlockMenuDefaultClassName', 'ryanwelcher/filters/getBlockMenuDefaultClassName/basic', filterMenuDefaultClassName );
