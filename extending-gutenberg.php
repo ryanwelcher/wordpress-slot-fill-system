1a6dc5df63fe72507308483b312d499053793759
Ryan Welcher <me@ryanwelcher.com>
2019-07-05 22:19:44 -0400

    Testing dashboard

--- extending-gutenberg.php
+++ extending-gutenberg.php
@@ -21,3 +21,31 @@ function enqueue_block_editor_assets() {
 	);
 }
 add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\enqueue_block_editor_assets' );
+
+//add_action( 'wp_dashboard_setup', __NAMESPACE__ . '\add_dashboard_widgets' );
+function add_dashboard_widgets() {
+	wp_add_dashboard_widget(
+		'extending_gutenberg_dashboard_widget',
+		'Extending Gutenberg',
+		__NAMESPACE__ . '\dashboard_widget_function'
+	);
+}
+
+function dashboard_widget_function() {
+	?>
+	<div id="extending-gutenberg-dashboard">Test</div>
+	<?php
+}
+
+add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\enqueue_dashboard_js' );
+
+function enqueue_dashboard_js() {
+	wp_enqueue_script( 'wp-element' );
+	wp_enqueue_script(
+		'eg-dashboard-widget',
+		plugin_dir_url( __FILE__ ) . '/build/main.js',
+		['wp-element'],
+		time(),
+		true
+	);
+}
