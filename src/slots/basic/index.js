5ea9c6247ea04d9ffbc9a969b693945f71b75803
Ryan Welcher <me@ryanwelcher.com>
2019-07-12 08:40:27 -0400

    Moving the combined stuff in to root index.js file

--- src/slots/basic/index.js
+++ src/slots/basic/index.js
@@ -1,1 +1,36 @@
-import './combined';
+/**
+ * Get registerPlugin.
+ */
+const { registerPlugin } = wp.plugins;
+
+/**
+ * Import our components that contain the SlotFills
+ */
+import { TenUp } from '../../svg/icons';
+import PluginBlockSettingsMenuGroupDemo from './plugin-block-settings-menu-item';
+import MyDocumentSettingDemo from './plugin-document-setting-panel';
+import MyButtonMoreMenuItemDemo from './plugin-more-menu-item';
+import PluginPostPublishPanelDemo from './plugin-post-publish-panel';
+import PluginPostStatusInfoDemo from './plugin-post-status-info';
+import PluginPrePublishPanelDemo from './plugin-pre-publish-panel';
+import PluginSidebarMoreMenuItemDemo from './plugin-sidebar-more-menu-item';
+
+/**
+ * One plugin to rule them all.
+ */
+const CombinedSlotFillsDemo = () => (
+	<>
+		<PluginBlockSettingsMenuGroupDemo />
+		<MyDocumentSettingDemo />
+		<MyButtonMoreMenuItemDemo />
+		<PluginPrePublishPanelDemo />
+		<PluginPostPublishPanelDemo />
+		<PluginPostStatusInfoDemo />
+		<PluginSidebarMoreMenuItemDemo />
+	</>
+);
+
+/**
+ * register the plugin.
+ */
+registerPlugin( 'extending-gutenberg', { render: CombinedSlotFillsDemo, icon: TenUp } );
