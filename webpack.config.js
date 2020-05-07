3431dd4211617518a4d30131b14c7cb937350cc3
Ryan Welcher <ryan.welcher@10up.com>
2020-05-07 00:22:25 -0400

    Build refactor

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require( 'path' );
+const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
+const WebpackBar = require( 'webpackbar' );
+const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
+
+module.exports = {
+	entry: {
+		blocks: './assets/js/block/index.js',
+		dashboard: './assets/js/dashboard/index.js',
+		settings: './assets/js/settings-page/index.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve( process.cwd(), 'dist' ),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				enforce: 'pre',
+				loader: 'eslint-loader',
+				options: {
+					fix: true,
+				},
+			},
+		],
+	},
+	plugins: [
+		// Clean the `dist` folder on build.
+		new CleanWebpackPlugin(),
+
+		// Fancy WebpackBar.
+		new WebpackBar(),
+
+		// Extract dependencies.
+		new DependencyExtractionWebpackPlugin(),
+	],
+};
