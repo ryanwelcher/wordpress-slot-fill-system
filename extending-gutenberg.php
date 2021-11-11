387cd6d86478dfe7c005e37883f5f095fbfcc41a
Ryan Welcher <ryan.welcher@automattic.com>
2021-11-11 17:59:12 -0600

    Add phpcs and cleanup php files.

--- extending-gutenberg.php
+++ extending-gutenberg.php
@@ -6,6 +6,8 @@
  * Author: Ryan Welcher
  * Author URI: www.ryanwelcher.com
  * Plugin URI: https://github.com/ryanwelcher/the-gutenberg-slotfill-system
+ *
+ * @package GutenbergSlotFillSystem
  */
 
 namespace GutenbergSlotFillSystem;
@@ -14,22 +16,24 @@ namespace GutenbergSlotFillSystem;
  * Enqueue the JS for our demos
  */
 function enqueue_block_editor_assets() {
-	wp_enqueue_script(
-		'extending-guteberg', // Handle.
-		plugin_dir_url( __FILE__ ) . '/build/index.js',
-		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor', 'wp-edit-post', 'word-count' ),
-		time()
-	);
+	$index_assets = plugin_dir_path( __FILE__ ) . 'build/index.asset.php';
+	if ( file_exists( $index_assets) ) {
+		$assets = require_once $index_assets;
+		\wp_enqueue_script(
+			'gutenberg-slot-fill-system', // Handle.
+			plugin_dir_url( __FILE__ ) . '/build/index.js',
+			$assets['dependencies'],
+			$assets['version'],
+			true
+		);
+	}
 }
-add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\enqueue_block_editor_assets' );
-
-
+\add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\enqueue_block_editor_assets' );
 
 
 /**
  * Create a new Dashboard Widget.
  */
-add_action( 'wp_dashboard_setup', __NAMESPACE__ . '\add_dashboard_widgets' );
 function add_dashboard_widgets() {
 	wp_add_dashboard_widget(
 		'extending_gutenberg_dashboard_widget',
@@ -39,27 +43,35 @@ function add_dashboard_widgets() {
 		}
 	);
 }
+\add_action( 'wp_dashboard_setup', __NAMESPACE__ . '\add_dashboard_widgets' );
 
 
 /**
  * Enqueue our JS on the dashboard page.
+ *
+ * @param string $hook The hook associated with the screen.
  */
-add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\enqueue_dashboard_js' );
 function enqueue_dashboard_js( $hook ) {
 	if ( 'index.php' === $hook ) {
-		$index_assets_php = plugin_dir_path( __FILE__ ) . 'build/index.asset.php';
-		if ( file_exists( $index_assets_php ) )  {
-			$assets = require_once $index_assets_php;
+		$dashboard_assets = plugin_dir_path( __FILE__ ) . 'build/dashboard.asset.php';
+		if ( file_exists( $dashboard_assets ) ) {
+			$assets = require_once $dashboard_assets;
 			wp_enqueue_script(
 				'eg-dashboard-widget',
 				plugin_dir_url( __FILE__ ) . '/build/dashboard.js',
-				[ 'wp-blocks', 'wp-i18n',  'wp-editor', 'wp-edit-post', 'wp-element', 'wp-components', 'wp-plugins' ],
-				time(),
+				$assets['dependencies'],
+				$assets['version'],
 				true
 			);
 		}
 	}
-
-	$user = wp_get_current_user();
-	wp_localize_script( 'eg-dashboard-widget', 'EB_DASH', [ 'user' => [ 'display_name' => $user->display_name ] ] );
+	$user = \wp_get_current_user();
+	\wp_localize_script(
+		'eg-dashboard-widget',
+		'EB_DASH',
+		array(
+			'user' => array( 'display_name' => $user->display_name ),
+		)
+	);
 }
+\add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\enqueue_dashboard_js' );
