fa2e3477e06a708757f0b10480d51f696e1c23e3
Ryan Welcher <ryan.welcher@automattic.com>
2022-04-22 14:53:14 -0400

    Update packages and simply the build process.

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
-module.exports = {
-	...defaultConfig,
-	entry: {
-		'edit-site': './src/slots/edit-site',
-		'edit-post': './src/slots/edit-post',
-		dashboard: './src/dashboard',
-	},
-};
